441bcc219f6db9815a1715d1e480c7176a6f3c7b
unknown <ethan.sayagh@gmail.com>
2020-11-25 10:20:13 +0200

    added particles

--- src/component/ContactTwo.js
+++ src/component/ContactTwo.js
@@ -3,12 +3,22 @@ import Reveal from "react-reveal/Reveal/";
 import SectionTitleTwo from "../component/Banner/SectionTitleTwo";
 import Form from "./Form";
 import Card from "./Card";
+import particles from "../assets/css/particle";
+import Particles from "react-particles-js";
+
 class ContactTwo extends Component {
   render() {
     return (
       <section className='contact-area contact-area-two bg_color' id='contacts'>
         <div className='container'>
           <div className='row'>
+            <div style={{ position: "absolute" }}></div>
+            <Particles
+              width='100vw'
+              height='100vh'
+              params={particles}
+              className='particle'
+            />
             <Card>
               <div className='section_title m-3'>
                 <SectionTitleTwo
